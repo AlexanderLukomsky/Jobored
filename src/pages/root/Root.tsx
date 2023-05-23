a11c7858e748d5b09e399bf2166d183e05f76b96
Alex Lukomsky <lukomsky.alexander@gmail.com>
2023-05-23 17:21:52 +0300

    feat: auth

--- src/pages/root/Root.tsx
+++ src/pages/root/Root.tsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from 'react';
 import { Flex } from '@mantine/core';
 import { Header } from 'components/header';
 import { Outlet } from 'react-router-dom';
+import { api } from 'services';
+import { Loader } from 'components/loader';
 import { layoutStyle } from './styles';
 
-export const Root = () => (
-  <Flex sx={{ minHeight: '100vh' }} bg="grayscale.4" direction="column">
-    <Header />
+export const Root = () => {
+  const [isAuth, setAuth] = useState(false);
 
-    <Flex sx={layoutStyle}>
-      <Outlet />
-    </Flex>
-  </Flex>
-);
+  useEffect(() => {
+    const auth = async () => {
+      await api.auth();
+      setAuth(true);
+    };
+
+    auth();
+  }, []);
+
+  if (isAuth) {
+    return (
+      <Flex sx={{ minHeight: '100vh' }} bg="grayscale.4" direction="column">
+        <Header />
+
+        <Flex sx={layoutStyle}>
+          <Outlet />
+        </Flex>
+      </Flex>
+    );
+  }
+
+  return <Loader />;
+};
